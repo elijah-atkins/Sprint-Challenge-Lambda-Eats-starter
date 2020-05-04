46ac45cd7b5aa3a535d485612c5885c099a1870b
Elijah Atkins <elijahorama@gmail.com>
2020-05-03 20:18:07 -0600

    name and dropdown added to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
-const Form = () =>{
-    return(
+const Form = () => {
+    return (
         <div className="pizza-order-form">
             <h1>Order Some Pizza Y'all</h1>
+            <form>
+                <label htmlFor="name" />
+        Name
+        <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    minLength="2"
+                />
+                <label htmlFor="size" />
+                    How big you want it?
+        <select id="size" name="size" >
+                    <option value="">--Please choose size--</option>
+                    <option value="Small">Smallr</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Large">Large</option>
+                    <option value="X-Large">X-Large</option>
+                </select>
+            </form>
         </div>
     )
 };
